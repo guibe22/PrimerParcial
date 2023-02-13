e524ac8619c79e4c593473a444bfc54b498d976b
guibe22 <galvezwilber22@gmail.com>
2023-02-13 18:56:51 -0400

    creando contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+public class Contexto:DbContext
+{
+    public DbSet<Libros> Libros { get; set; }
+
+    public Contexto( DbContextOptions<Contexto> option) :base(option)
+    {
+        
+    }
+    
+}
